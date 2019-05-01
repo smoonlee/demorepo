70a530dc12541c71a761ca7ae31c88ae9348dd10
Simon Lee <simon@leefamily.uk.net>
2019-05-01 13:47:44 +0100

    Update and rename setup_script.sh to provisioning_script.sh

---
+++ provisioning_script.sh
@@ -0,0 +1,24 @@
+# Script Name: Demo Installation Script
+# Author: Simon Lee
+# Date: May 2019
+
+# Clear Screen  
+Clear
+
+#!/bin/bash
+# Check Operating System Type
+
+# Docker Container
+DockerContainer=/.dockerenv
+if [ -f $DockerContainer ]; then
+# https://unix.stackexchange.com/questions/432816/grab-id-of-os-from-etc-os-release
+echo "Running in a Docker Container"
+if [ -f /etc/os-release]; then
+. /etc/os-release
+echo "Operating System: $VERSION
+fi
+fi
+
+# Debian/Ubuntu 
+
+# CentOS/RHEL

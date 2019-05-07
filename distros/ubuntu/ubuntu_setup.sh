49c500701ba97fa9d29dc23373ab883f619a8701
Simon Lee <simon@leefamily.uk.net>
2019-05-07 12:28:58 +0100

    Create ubuntu_setup.sh

---
+++ distros/ubuntu/ubuntu_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Clear Screen
+/usr/bin/clear
+
+echo "#############################################"
+echo "#                                           #"
+echo "#        GitHub : Demo Repository           #"
+echo "#   Ubuntu Initial Package & Repo Updates   #"
+echo "#                                           #"
+echo "#############################################"
+
+echo ""
+apt udate && apt upgrade -y
